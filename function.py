f0e62e89cf2dfe0f1a6907367f9568789695d380
ericwang1107 <104775830+ericwang1107@users.noreply.github.com>
2022-05-05 23:15:07 +0800

    eric

--- function.py
+++ function.py
@@ -3,3 +3,14 @@ def fun1(p1=1,p2=2):
 print(fun1(2,3))
 print(fun1("abc","4"))
 print(fun1())
+
+#無限參數
+def say(*msgs):
+    #Tuple處理
+    for msg in msgs:
+        print(msg)
+#call
+say("a","b","c")
+
+#呼叫函式以參數名稱對應
+fun1(p2=9,p1=3)
---
+++ import.py
@@ -0,0 +1,10 @@
+import function as f
+import sys as s
+import 變數類型操作基礎範例.int_string
+print("載入模組範例開始")
+print(f.fun1(p2=3,p1=2))
+
+#調整搜尋模組的路徑
+#模組的搜尋路徑
+print(s.path)
+s.path.append("變數類型操作基礎範例")
